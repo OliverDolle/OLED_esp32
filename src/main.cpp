8b5f5f39982bd3a85c8a6db6b85418e5d21b12f0
OliverDolle <oliver2006nielsen@gmail.com>
2025-08-05 23:33:20 +0200

    Added change of text based on timer and list

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,10 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include "center_text.h"
+#include <list>
+#include <string>
+using std::list;
+using std::string;
 
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
@@ -14,6 +18,8 @@
 #define OLED_SCL 22
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+
 
 void setup() {
   Serial.begin(115200);
@@ -29,6 +35,31 @@ void setup() {
   centerPrint("Hello, World!", 1, 0); // Example usage of centerPrint
 }
 
+unsigned long previousMillis = 0;
+const unsigned long interval = 1000; 
+
+list<string> textList = {
+    "Hello, World!",
+    "This is a test.",
+    "Centering text is fun!",
+    "Adafruit SSD1306",
+    "ESP32 with OLED"
+};
+
+int i = 0;
+
 void loop() {
-  // Static image, therefore no loop yet
+  unsigned long currentMillis = millis();
+  
+  if (currentMillis - previousMillis >= interval) {
+      previousMillis = currentMillis;
+
+    if (i > 4) {
+        i = 0; // Reset index if it exceeds the list size
+    }
+    auto it = textList.begin();
+    std::advance(it, i);
+    centerPrint(*it, 1, 0); // Print centered text
+    i++;
+  }
 }
